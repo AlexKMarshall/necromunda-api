2b057ae749d91705ceba45d2341d208269139d8d
Alex Marshall <alex.k.marshall83@gmail.com>
2020-11-21 18:36:09 +0000

    Convert Gang to use zod validation

--- src/domain/gang/gang.type.ts
+++ src/domain/gang/gang.type.ts
@@ -1,16 +1,19 @@
 import Joi from "joi";
-import { Faction } from "../faction/faction.type";
+import * as z from "zod";
+import { factionSchema } from "../faction/faction.type";
 
-export type Gang = {
-  _id?: string;
-  name: string;
-  faction: Faction;
-  userId: string;
-};
+export const gangSchema = z.object({
+  name: z.string(),
+  userId: z.string(),
+  faction: factionSchema,
+  _id: z.string().uuid(),
+});
 
-export type GangInboundDTO = Omit<Gang, "_id" | "faction"> & {
-  faction: string;
-};
+export type Gang = z.infer<typeof gangSchema>;
+export const gangInboundSchema = gangSchema
+  .omit({ _id: true })
+  .extend({ faction: factionSchema.pick({ _id: true }) });
+export type GangInboundDTO = z.infer<typeof gangInboundSchema>;
 
 export const gangValidationSchema = Joi.object({
   name: Joi.string().required(),
