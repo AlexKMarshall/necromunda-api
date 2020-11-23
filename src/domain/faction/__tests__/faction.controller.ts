a1e5e6fbecbc32e21d3d4301387a5c1f06fbdc6d
Alex Marshall <alex.k.marshall83@gmail.com>
2020-11-23 19:46:43 +0000

    Create buildFactionInbound test object factory

--- src/domain/faction/__tests__/faction.controller.ts
+++ src/domain/faction/__tests__/faction.controller.ts
@@ -2,6 +2,17 @@ import * as factionController from "../faction.controller";
 import * as dbUtils from "../../../test/db-utils";
 import * as E from "fp-ts/lib/Either";
 import { FactionModel } from "../faction.model";
+import { FactionInbound } from "../faction.type";
+import faker from "faker";
+
+function buildFactionInbound(
+  overrides?: Partial<FactionInbound>
+): FactionInbound {
+  return {
+    name: faker.company.companyName(),
+    ...overrides,
+  };
+}
 
 beforeAll(dbUtils.connectMongoose);
 afterAll(dbUtils.disconnectMongoose);
@@ -10,21 +21,20 @@ beforeEach(dbUtils.clearDatabase);
 
 describe("factionController", () => {
   test("postFaction creates a faction", async () => {
-    const name: string = "My Faction";
+    const faction = buildFactionInbound();
 
-    const trigger = factionController.postFaction({ name });
-
+    const trigger = factionController.postFaction(faction);
     const result = await trigger();
 
     expect(E.isRight(result)).toBe(true);
 
     if (E.isRight(result)) {
-      expect(result.right).toEqual(expect.objectContaining({ name }));
+      expect(result.right).toEqual(expect.objectContaining(faction));
     }
   });
   test("postFaction without a valid faction returns exception", async () => {
-    const trigger = factionController.postFaction({});
-
+    const empty = {};
+    const trigger = factionController.postFaction(empty);
     const result = await trigger();
 
     expect(E.isLeft(result)).toBe(true);
@@ -39,13 +49,35 @@ describe("factionController", () => {
       `);
     }
   });
+  xtest("postFaction with an existing name returns exception", async () => {
+    const existingFaction = buildFactionInbound({ name: "FAKE_FACTION" });
+    await FactionModel.create(existingFaction);
+
+    const trigger = factionController.postFaction(existingFaction);
+    const result = await trigger();
+
+    console.log(result);
+
+    expect(E.isLeft(result)).toBe(true);
+
+    if (E.isLeft(result)) {
+      expect(result.left).toMatchInlineSnapshot(`
+        [Error: 1 validation issue(s)
+
+          Issue #0: invalid_type at name
+          Required
+        ]
+      `);
+    }
+  });
   test("getFactions returns some factions", async () => {
-    const factionOne = { name: "faction1" };
-    const factionTwo = { name: "faction2" };
+    const [factionOne, factionTwo] = [
+      buildFactionInbound(),
+      buildFactionInbound(),
+    ];
     await FactionModel.create([factionOne, factionTwo]);
 
     const trigger = factionController.getFactions();
-
     const result = await trigger();
 
     expect(E.isRight(result)).toBe(true);
