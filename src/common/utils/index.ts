d66a5e81029c51094eefd35859e51787d982c9e6
Alex Marshall <alex.k.marshall83@gmail.com>
2020-11-27 18:32:00 +0000

    Test commit

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,4 +1,4 @@
-export function removeProp<T, K extends keyof T>(prop: K, obj: T): Omit<T, K> {
+export function removeProp<T, K extends keyof T>(obj: T, prop: K): Omit<T, K> {
   // eslint-disable-next-line no-unused-vars
   const { [prop]: _, ...rest } = obj;
   return rest;
